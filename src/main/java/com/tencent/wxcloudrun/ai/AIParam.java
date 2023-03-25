574e91c12d6f7d49b14a5b874673fd3cee7bf606
cnsource <2274850295@qq.com>
2023-03-25 16:29:27 +0800

    fix: 去除lombok依赖

--- src/main/java/com/tencent/wxcloudrun/ai/AIParam.java
+++ src/main/java/com/tencent/wxcloudrun/ai/AIParam.java
@@ -4,9 +4,41 @@ import lombok.Data;
 
 import java.util.List;
 
-@Data
 public class AIParam {
     private String model = "abab5-chat";
     private Integer tokens_to_generate = 512;
     private List<Message> messages;
+
+    public AIParam() {
+    }
+
+    public AIParam(String model, Integer tokens_to_generate, List<Message> messages) {
+        this.model = model;
+        this.tokens_to_generate = tokens_to_generate;
+        this.messages = messages;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Integer getTokens_to_generate() {
+        return tokens_to_generate;
+    }
+
+    public void setTokens_to_generate(Integer tokens_to_generate) {
+        this.tokens_to_generate = tokens_to_generate;
+    }
+
+    public List<Message> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(List<Message> messages) {
+        this.messages = messages;
+    }
 }
--- src/main/java/com/tencent/wxcloudrun/ai/Message.java
+++ src/main/java/com/tencent/wxcloudrun/ai/Message.java
@@ -2,8 +2,32 @@ package com.tencent.wxcloudrun.ai;
 
 import lombok.Data;
 
-@Data
+
 public class Message {
     private String sender_type = "";
     private String text = "";
+
+    public Message() {
+    }
+
+    public Message(String sender_type, String text) {
+        this.sender_type = sender_type;
+        this.text = text;
+    }
+
+    public String getSender_type() {
+        return sender_type;
+    }
+
+    public void setSender_type(String sender_type) {
+        this.sender_type = sender_type;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
