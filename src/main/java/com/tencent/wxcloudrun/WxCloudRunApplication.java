6caaef11020b57909501087fdd322d25b9c3e58f
cnsource <2274850295@qq.com>
2023-03-25 17:23:34 +0800

    fix: 测试1

--- src/main/java/com/tencent/wxcloudrun/WxCloudRunApplication.java
+++ src/main/java/com/tencent/wxcloudrun/WxCloudRunApplication.java
@@ -1,13 +1,11 @@
 package com.tencent.wxcloudrun;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
-@MapperScan(basePackages = {"com.tencent.wxcloudrun.dao"})
-public class WxCloudRunApplication {  
-
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class WxCloudRunApplication {
   public static void main(String[] args) {
     SpringApplication.run(WxCloudRunApplication.class, args);
   }
